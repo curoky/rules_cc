f28b31e080234d736009214084e81593db3206cd
curoky <cccuroky@gmail.com>
2019-01-27 23:19:14 +0800

    [bazel]: upgrade folly/wangle/fbthrift

--- third_party/fbthrift/extra/gen-cpp2/reflection_visitation.h
+++ third_party/fbthrift/extra/gen-cpp2/reflection_visitation.h
@@ -5,15 +5,5 @@
  *  @generated
  */
 #pragma once
-
-#include <thrift/lib/cpp2/gen/module_constants_h.h>
-
-#include "thrift/lib/thrift/gen-cpp2/reflection_types.h"
-
-namespace apache { namespace thrift { namespace reflection {
-
-struct reflection_constants {
-
-};
-
-}}} // apache::thrift::reflection
+#include "thrift/lib/thrift/gen-cpp2/reflection_for_each_field.h"
+#include "thrift/lib/thrift/gen-cpp2/reflection_visit_union.h"
